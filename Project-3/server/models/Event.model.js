abe541bdddd2569e68bf892795a4051e9e6fd2c6
Alejandro Martinez <alejandro.mzrz@gmail.com>
2022-03-11 17:57:21 +0100

    cambio de viernes tarde

--- Project-3/server/models/Event.model.js
+++ Project-3/server/models/Event.model.js
@@ -6,8 +6,8 @@ const eventSchema = new Schema(
 
         owner: [{type: Schema.Types.ObjectId, ref:'User'}],
         title: {type: String, required: true},
-        description: {type: String, maxlength: 400},
-        image: {type: String, default:"https://cdn-icons-png.flaticon.com/512/1077/1077114.png"},
+        description: {type: String, maxlength: 400, required: true},
+        image: {type: String, default:"https://cdn-icons.flaticon.com/png/512/2397/premium/2397338.png?token=exp=1647016518~hmac=2af01a418d7d54a5faaa59e3f4938a1b"},
         date: {type: Date},
         schedule: {type: Date},
         artists: [{type: Schema.Types.ObjectId, ref:'User'}],
