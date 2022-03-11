f2f6b83c998a9d9e690b87f81ffd4facc96a6699
Pablo López <p.lopezmunoz@gmail.com>
2022-03-11 14:50:17 +0100

    users name

--- Project-3/client/src/pages/AllUsers.jsx
+++ Project-3/client/src/pages/AllUsers.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import Searchbar from '../components/Searchbar/Searchbar';
-import Users from '../components/Users/Users';
-import { Link } from 'react-router-dom';
+import UserCard from '../components/UserCard/UserCard'
+import axios from 'axios';
 import ScrollUpBtn from '../components/ScrollUpBtn/ScrollUpBtn';
-import Navbar from '../components/Navbar/Navbar';
+import { Link } from 'react-router-dom';
 
 function AllProfiles(props) {
+    const[users,setUsers]=useState([]);
+    const[filteredUsers,setFilteredUsers]=useState([]);
+
+    useEffect(()=>{
+    axios.get(`${process.env.REACT_APP_SERVER_URL}/api/users`)
+    .then((response)=>{
+        setUsers(response.data)
+        setFilteredUsers(response.data)
+    })
+    .catch((err)=>console.log('CAGADAAAAAAAA',err))
+},[])
+
+    function search(str){
+        const newList=users.filter((user)=>{
+            return user.username.toLowerCase().includes(str.toLowerCase())
+    })
+    setFilteredUsers(newList)
+    }
+
     return (
-        <div className='margin-top'>
-        <div className="flex-center">
-            <img src="../../ios-arrow-back-logo-icon-png-svg (1).png" alt="arrow back" className="goBackBtn"/>
-            <Link exact to="/main"> Go back</Link>
-        </div>
+        <div>
+            <div className="flex-center">
+                <img src="../../ios-arrow-back-logo-icon-png-svg (1).png" alt="arrow back" className="goBackBtn"/>
+                <Link exact to="/main"> Go back</Link>
+            </div>
             <h1>Check all the profiles</h1>
+            <Searchbar filter={search}/>
             <ScrollUpBtn />
-            <Searchbar/>
-            <Users />
-            <Navbar />
+            <div>
+                {filteredUsers.map((user)=>(
+                    <UserCard key={user._id} profile={user}/>
+                ))}
+            </div>
         </div>
     );
 }
