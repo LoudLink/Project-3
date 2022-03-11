4faa7776680daf7177881fd4c009498b1c3b3447
Santiago <santiagofgaray@gmail.com>
2022-03-11 16:54:29 +0100

    update AllAnnouncements, AllUsers, AllEvents

--- Project-3/client/src/pages/AllEvents.jsx
+++ Project-3/client/src/pages/AllEvents.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import EventCard from '../components/Events/EventCard'
 import { Link } from 'react-router-dom';
 import Events from '../components/Events/Events'
 import Navbar from '../components/Navbar/Navbar';
@@ -6,6 +8,27 @@ import ScrollUpBtn from '../components/ScrollUpBtn/ScrollUpBtn';
 import Searchbar from '../components/Searchbar/Searchbar';
 
 function AllEvents(props) {
+    const[events,setEvents]=useState([])
+    const[eventsFiltered,setFilteredEvents]=useState([])
+
+    useEffect(()=>{
+        axios.get(`${process.env.REACT_APP_SERVER_URL}/api/events`)
+        .then((response)=>{
+            setEvents(response.data);
+            setFilteredEvents(response.data);
+        })
+        .catch((err)=>console.log(err))
+    },[])
+
+    function search(str){
+        const strLength = str.length
+        const newList = events.slice().filter((el)=> {
+        return el.title.slice(0,strLength).toLowerCase() === str.slice(0,strLength).toLowerCase()
+        })
+        setFilteredEvents(newList)
+    }
+
+
     return (
         <div className='margin-top'>
             <div className="flex-center">
@@ -14,10 +37,16 @@ function AllEvents(props) {
             </div>
             <h1>Check all the events</h1>
             <ScrollUpBtn />
-            <Searchbar/>
-            <Link exact to="/events/create-event">Create an event</Link>
+            <Searchbar filter={search} />
             <Events />
             <Navbar />
+            <div>
+                {eventsFiltered.map((event)=>(
+                    <EventCard key={event._id} event={event} />
+                ))}
+            </div>
+            <Link exact to="/events/create-event">Create an event</Link>
+            
         </div>
     );
 }
