312110299e4b09e574be6a1b44865a8e23cedd26
Pablo López <p.lopezmunoz@gmail.com>
2022-03-13 14:38:08 +0100

    delete events, annos and user at the same time

--- Project-3/server/routes/user.routes.js
+++ Project-3/server/routes/user.routes.js
@@ -79,7 +79,142 @@ router.delete("/:userId", (req, res) => {
     return;
   }
 
+  let countAnno = 0;
+  let countEve = 0;
+  const userIdCopy = userId;
+
+  function deleteAnnouncements() {
+    Announcement.find()
+    .populate("owner")
+    .then((anno) => {
+      anno.map((annito) => {
+        if(annito.owner.toString().includes(userIdCopy)){
+          Announcement.findByIdAndDelete(annito._id)
+          .then(
+            console.log("Announcement deleted")
+          )
+        }
+        else {
+          countAnno += 1;
+          console.log("Announcements checked: ", countAnno);
+        }
+      })
+    })
+  }
+
+  function deleteEvents() {
+    Event.find()
+    .populate("owner")
+    .then((eve) => {
+      eve.map((evito) => {
+        if(evito.owner.toString().includes(userIdCopy)){
+          Event.findByIdAndDelete(evito._id)
+          .then(
+            console.log("Event deleted")
+          )
+        }
+        else {
+          countEve += 1;
+          console.log("Events checked: ", countEve);
+        }
+      })
+    })
+  }
+
+  function deleteUser() {
+    User.findByIdAndDelete(userId)
+                          .then(() => {
+                            res.status(200).json({
+                              message: `User with ${userId} is removed successfully.`,
+                            });
+                          })
+                          .catch((err) => res.json(err))
+                        
+  }
+
+  function deleteAnnouncementsAndEvents() {
+    deleteAnnouncements()
+    deleteEvents();
+  }
+
+  deleteAnnouncementsAndEvents()
+  setTimeout(() => {
+    deleteUser()
+  }, 1000)
+})
+
+  /*
   Announcement.find()
+    .populate("owner")
+    .then((anno) => {
+      Promise.all(
+        anno.map((annito) => {
+          if (annito.owner.toString().includes(userId)) {
+            Announcement.findByIdAndDelete(annito._id).then(
+              Event.find()
+                .populate("owner")
+                .then((eve) => {
+                  eve.map((evito) => {
+                    if (evito.owner.toString().includes(userId)) {
+                      Event.findByIdAndDelete(evito._id).then(
+                        User.findByIdAndDelete(userId)
+                          .then(() => {
+                            res.status(200).json({
+                              message: `User with ${userId} is removed successfully.`,
+                            });
+                          })
+                          .catch((err) => res.json(err))
+                      );
+                    }
+                  });
+                })
+            );
+          } 
+          
+          else {
+            countAnno += 1;
+            console.log("Announcements checked: ", countAnno);
+          }
+        })
+      ).then(
+        Event.find()
+          .populate("owner")
+          .then((eve) => {
+            Promise.all(
+              eve.map((evito) => {
+                if (evito.owner.toString().includes(userId)) {
+                  Event.findByIdAndDelete(evito._id).then(
+                    User.findByIdAndDelete(userId)
+                      .then(() => {
+                        res.status(200).json({
+                          message: `User with ${userId} is removed successfully.`,
+                        });
+                      })
+                      .catch((err) => res.json(err))
+                  );
+                } else {
+                  countEve += 1;
+                  console.log("Events checked: ", countEve);
+                }
+              })
+            )
+            .then(
+              User.findByIdAndDelete(userId)
+                .then(() => {
+                  res.status(200).json({
+                    message: `User with ${userId} is removed successfully.`,
+                  });
+                })
+                .catch((err) => res.json(err))
+            );
+          })
+      );
+    });
+});
+
+*/
+
+/*Announcement.find()
     .populate("owner")
     .then((anno) => {
       anno.map((annito) => {
@@ -105,7 +240,6 @@ router.delete("/:userId", (req, res) => {
           );
         }
       });
-    });
-});
+    }); */
 
 module.exports = router;
